9f4a6d59b01d80145f1c25868ea69a1fc8d6451c
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-16 19:51:44 +0200

    .map Accordion + css change

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
 import './style.css';
 import { useState } from 'react';
 
+/** FAQ content */
+const faq = [
+  {
+    title: 'Is this a good product?',
+    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
+  },
+  {
+    title: 'How much does it cost?',
+    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
+  },
+  {
+    title: 'When can I get it?',
+    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
+  },
+];
+
 function Accordion({title, content}) {
 
-  const [show, setShow] = useState("+");
+  const [state, setState] = useState("+");
 
   function handleShow() {
-    setShow(show === "+"? "-" : "+");
+    setState(state === "+"? "-" : "+");
   }
-
 
   return (
     <section className='accordion'>
       <div className="accordion-front">
         <h3 className='accordion-title'>{title}</h3>
-          <button className='toggle' onClick={handleShow}>{show}</button>
+        <button className='toggle' onClick={handleShow}>{state}</button>
       </div>
-
-      {show === "-" && (
-      <p className='accordion-content'>{content}</p>
-      )}
+      {state === "-" && <p className='accordion-content'>{content}</p>}
     </section>
   );
 }
 
 export default function App() {
-
-  const faq = [
-    {
-      title: 'Is this a good product?',
-      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
-    },
-    {
-      title: 'How much does it cost?',
-      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
-    },
-    {
-      title: 'When can I get it?',
-      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?'
-    }
-  ];
 
   return (
     <div className="App">
@@ -48,26 +45,9 @@ export default function App() {
       <section className="faq">
         <h2>Frequently Asked Questions</h2>
 
-        <Accordion key={0} title={faq[1].title} content={faq[0].content}/>
-
-        <section className='accordion'>
-          <div className="accordion-front">
-            <h3 className='accordion-title'>How much does it cost?</h3>
-            <button className='toggle'>+</button>
-          </div>
-
-          <p className='accordion-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
-        </section>
-
-        <section className='accordion'>
-          <div className="accordion-front">
-            <h3 className='accordion-title'>When can I get it?</h3>
-            <button className='toggle'>+</button>
-          </div>
-
-          <p className='accordion-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
-        </section>
-
+        {faq.map(
+          (question, index) => <Accordion key={index} title={question.title} content={question.content}/>
+        )}
         
       </section>
     </div>
