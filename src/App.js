56fd6f358efe6cbeb1f777620f9771af940cbd56
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-16 17:51:09 +0200

    html structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './style.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hellow Wordl !</h1>
+      <h1>Project 2: FAQ/Accordion</h1>
+
+      <div className="faq">
+        <h2>Frequently Asked Questions</h2>
+        <section>
+          <h3>Is this a good product?</h3>
+          <button>+</button>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+        </section>
+
+        <section>
+          <h3>How much does it cost?</h3>
+          <button>+</button>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+        </section>
+
+        <section>
+          <h3>When can I get it?</h3>
+          <button>+</button>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+        </section>
+
+        
+      </div>
     </div>
   );
 }
-
-export default App;
