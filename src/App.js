0cc69b8f7ee762f7786707de61f381b2f1276086
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-16 18:55:16 +0200

    css + modif html structure

--- src/App.js
+++ src/App.js
@@ -7,25 +7,31 @@ export default function App() {
 
       <div className="faq">
         <h2>Frequently Asked Questions</h2>
-        <section>
-          <h3>Is this a good product?</h3>
-          <button>+</button>
+        <section className='accordion'>
+          <div className="accordion-front">
+            <h3 className='accordion-title'>Is this a good product?</h3>
+            <button className='toggle'>+</button>
+          </div>
 
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+          <p className='accordion-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
         </section>
 
-        <section>
-          <h3>How much does it cost?</h3>
-          <button>+</button>
+        <section className='accordion'>
+          <div className="accordion-front">
+            <h3 className='accordion-title'>How much does it cost?</h3>
+            <button className='toggle'>+</button>
+          </div>
 
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+          <p className='accordion-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
         </section>
 
-        <section>
-          <h3>When can I get it?</h3>
-          <button>+</button>
+        <section className='accordion'>
+          <div className="accordion-front">
+            <h3 className='accordion-title'>When can I get it?</h3>
+            <button className='toggle'>+</button>
+          </div>
 
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
+          <p className='accordion-content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nisi magni totam explicabo quam blanditiis unde excepturi! Quam quibusdam, labore atque culpa magni deleniti quia laudantium, provident illum veritatis facilis?</p>
         </section>
 
         
